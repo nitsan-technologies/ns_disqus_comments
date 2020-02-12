a85bfb0080efb64abf3a72ad3a92595c27b21827
NITSAN Technologies <sanjay@nitsan.in>
2020-02-12 15:48:01 +0530

    [Update] Maintenance Release with TYPO3 v10 Support

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => '[NITSAN] Disqus.com Plugin',
-    'description' => 'Easily configure Disqus.com plugin to your TYPO3 pages, news, blogs etc., Disqus is world one of the most popular comment system. Live-Demo: https://demo.t3terminal.com/t3t-extensions/disqus-comment/ You can download PRO version for more-features & free-support at ttps://t3terminal.com/typo3-disqus-comment-plugin-free/',
+    'description' => 'Easily configure Disqus.com plugin to your TYPO3 pages, news, blogs etc., Disqus is world one of the most popular comment system. LIVE Demo: https://demo.t3terminal.com/t3t-extensions/disqus-comment/ PRO version: https://t3terminal.com/typo3-disqus-comment-extension',
     'category' => 'plugin',
     'author' => 'T3:Milan Rathod, T3:Keval Pandya, QA:Vandna Kalivada',
 	'author_email' => 'sanjay@nitsan.in',
@@ -12,10 +12,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.1.0',
+    'version' => '1.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.0.0-9.5.99',
+            'typo3' => '6.0.0-10.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
