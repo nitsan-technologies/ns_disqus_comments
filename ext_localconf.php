539acbec82948251c85ad7a1556f5945ccf89dd3
NITSAN Technologies <sanjay@nitsan.in>
2021-03-16 17:27:27 +0530

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -16,7 +16,7 @@ defined('TYPO3_MODE') || die('Access denied.');
 if (version_compare(TYPO3_branch, '7.0', '>')) {
     if (TYPO3_MODE === 'BE') {
         $icons = [
-            'ext-ns-discuss-icon' => 'user_plugin_comment.svg',
+            'ext-ns-discuss-icon' => 'ns_disqus_comments.svg',
         ];
         $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
         foreach ($icons as $identifier => $path) {
