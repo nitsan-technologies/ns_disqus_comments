6827e43e675388ce29ec6db1f730804d61e55816
NITSAN Technologies <sanjay@nitsan.in>
2022-08-09 12:48:48 +0530

    [TASK] Change URL of t3terminal into t3planet

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
     'title' => '[NITSAN] Disqus.com Plugin',
-    'description' => 'Easily configure Disqus.com plugin to your TYPO3 pages, news, blogs etc., Disqus is world one of the most popular comment system. LIVE Demo: https://demo.t3terminal.com/t3t-extensions/disqus-comment/ PRO version: https://t3terminal.com/typo3-disqus-comment-extension',
+    'description' => 'Easily configure Disqus.com plugin to your TYPO3 pages, news, blogs etc., Disqus is world one of the most popular comment system. LIVE Demo: https://demo.t3terminal.com/t3t-extensions/disqus-comment/ PRO version: https://t3planet.com/typo3-disqus-comment-extension',
     'category' => 'plugin',
     'author' => 'NITSAN Technologies Pvt Ltd',
     'author_email' => 'sanjay@nitsan.in',
