025aaa5b09537a583e1b284082f7b0535e760f16
Krupali <krupali.nitsan@gmail.com>
2024-07-31 13:37:13 +0530

    [TODO] version compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '12.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0-12.4.99',
+            'typo3' => '12.0.0-13.0.99',
         ],
         'conflicts' => [],
         'suggests' => [],
