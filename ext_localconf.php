9b2f58787b3799ce376e7292377e224ae592770a
nitsan-technologies <alfaz.nitsan@gmail.com>
2026-01-19 18:59:52 +0530

    [TASK]Work On Code Review

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,11 +4,6 @@ defined('TYPO3') || die('Access denied.');
 
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 use Ns\NsDisqusComments\Controller\NsDisqusCommentsController;
-use TYPO3\CMS\Core\Utility\VersionNumberUtility;
-
-$versionNumber =  VersionNumberUtility::convertVersionStringToArray(VersionNumberUtility::getCurrentTypo3Version());
-
-if ($versionNumber['version_main'] <= '12') {
     ExtensionUtility::configurePlugin(
         'NsDisqusComments',
         'Comment',
@@ -20,17 +15,3 @@ if ($versionNumber['version_main'] <= '12') {
             NsDisqusCommentsController::class => 'disqusComments',
         ]
     );
-}else{
-    ExtensionUtility::configurePlugin(
-        'NsDisqusComments',
-        'Comment',
-        [
-            NsDisqusCommentsController::class => 'disqusComments',
-        ],
-        // non-cacheable actions
-        [
-            NsDisqusCommentsController::class => 'disqusComments',
-        ],
-        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
-    );
-}
